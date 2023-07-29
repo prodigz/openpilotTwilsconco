thatcobbyguy/openpilot

torque controller NNFF
enable live-torque NNFF

Better error response and friction factor use

unsubmodule cereal

log nnff inputs

Update interfaces.py

error downscaling

Less error downscale dead zone

Constant error downscale

downscale other error responses

Update latcontrol_torque.py

No double friction

no constant error downscale

fix error downscaling

cleanup

better logging; better live-torque

Update latcontrol_torque.py

cleanup; downscale friction too

Update latcontrol_torque.py

disable live kf/friction; faster error scale filter

cleanup

selfdrive/car/interfaces.py
@@ -1,8 +1,10 @@
 import yaml
+import numpy as np
 import os
 import time
 from abc import abstractmethod, ABC
-from typing import Any, Dict, Optional, Tuple, List, Callable
+from json import load
+from typing import Any, Dict, Optional, Tuple, List, Callable, Union
 
 from cereal import car
 from common.basedir import BASEDIR
@@ -54,12 +56,93 @@ def get_torque_params(candidate):
   return {key: out[i] for i, key in enumerate(params['legend'])}
 
 
+# lateral neural network feedforward
+class FluxModel:
+  # dict used to rename activation functions whose names aren't valid python identifiers
+  activation_function_names = {'σ': 'sigmoid'}
+  def __init__(self, params_file, zero_bias=False):
+    with open(params_file, "r") as f:
+      params = load(f)
+
+    self.input_size = params["input_size"]
+    self.output_size = params["output_size"]
+    self.input_mean = np.array(params["input_mean"], dtype=np.float32).T
+    self.input_std = np.array(params["input_std"], dtype=np.float32).T
+    self.layers = []
+
+    for layer_params in params["layers"]:
+      W = np.array(layer_params[next(key for key in layer_params.keys() if key.endswith('_W'))], dtype=np.float32).T
+      b = np.array(layer_params[next(key for key in layer_params.keys() if key.endswith('_b'))], dtype=np.float32).T
+      if zero_bias:
+        b = np.zeros_like(b)
+      activation = layer_params["activation"]
+      for k, v in self.activation_function_names.items():
+        activation = activation.replace(k, v)
+      self.layers.append((W, b, activation))
+    
+    self.validate_layers()
+    
+  # Begin activation functions.
+  # These are called by name using the keys in the model json file
+  def sigmoid(self, x):
+    return 1 / (1 + np.exp(-x))
+
+  def identity(self, x):
+    return x
+  # End activation functions
+
+  def forward(self, x):
+    for W, b, activation in self.layers:
+      x = getattr(self, activation)(x.dot(W) + b)
+    return x
+
+  def evaluate(self, input_array):
+    in_len = len(input_array)
+    if in_len != self.input_size:
+      # If the input is length 2-4, then it's a simplified evaluation.
+      # In that case, need to add on zeros to fill out the input array to match the correct length.
+      if 2 <= in_len:
+        input_array = input_array + [0] * (self.input_size - in_len)
+      else:
+        raise ValueError(f"Input array length {len(input_array)} must be length 2 or greater")
+        
+    input_array = np.array(input_array, dtype=np.float32)#.reshape(1, -1)
+
+    # Rescale the input array using the input_mean and input_std
+    input_array = (input_array - self.input_mean) / self.input_std
+
+    output_array = self.forward(input_array)
+
+    return float(output_array[0, 0])
+  
+  def validate_layers(self):
+    for W, b, activation in self.layers:
+      if not hasattr(self, activation):
+        raise ValueError(f"Unknown activation: {activation}")
+  
+def get_nn_ff_model_path(car):
+  return f"/data/openpilot/selfdrive/car/torque_data/lat_models/{car}.json"
+
+def has_nn_ff(car):
+  model_path = get_nn_ff_model_path(car)
+  if os.path.isfile(model_path):
+    return True
+  else:
+    return False
+  
+def initialize_nnff(car) -> Union[FluxModel, None]:
+  model = None
+  if has_nn_ff(car):
+    model = FluxModel(get_nn_ff_model_path(car))
+  return model
+
 # generic car and radar interfaces
 
 class CarInterfaceBase(ABC):
   def __init__(self, CP, CarController, CarState):
     self.CP = CP
     self.VM = VehicleModel(CP)
+    self.has_lat_torque_nnff = self.initialize_lat_torque_nnff(CP.carFingerprint)
 
     self.frame = 0
     self.steering_unpressed = 0
@@ -83,6 +166,13 @@ class CarInterfaceBase(ABC):
     self.CC = None
     if CarController is not None:
       self.CC = CarController(self.cp.dbc_name, CP, self.VM)
+      
+  def get_ff_nn(self, x):
+    return self.lat_torque_nnff_model.evaluate(x)
+  
+  def initialize_lat_torque_nnff(self, car):
+    self.lat_torque_nnff_model = initialize_nnff(car)
+    return (self.lat_torque_nnff_model is not None)
 
   @staticmethod
   def get_pid_accel_limits(CP, current_speed, cruise_speed):
@@ -99,6 +189,9 @@ class CarInterfaceBase(ABC):
   def get_params(cls, candidate: str, fingerprint: Dict[int, Dict[int, int]], car_fw: List[car.CarParams.CarFw], experimental_long: bool, docs: bool):
     ret = CarInterfaceBase.get_std_params(candidate)
     ret = cls._get_params(ret, candidate, fingerprint, car_fw, experimental_long, docs)
+    
+    # Enable torque controller for all cars
+    CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     # Set common params using fields set by the car interface
     # TODO: get actual value, for now starting with reasonable value for

prodigz/openpilotTwilsconco

selfdrive/car/interfaces.py
@@ -1,8 +1,10 @@
 import yaml
+import numpy as np
 import os
 import time
 from abc import abstractmethod, ABC
-from typing import Any, Dict, Optional, Tuple, List, Callable
+from json import load
+from typing import Any, Dict, Optional, Tuple, List, Callable, Union
 
 from cereal import car
 from common.basedir import BASEDIR
@@ -54,12 +56,93 @@ def get_torque_params(candidate):
   return {key: out[i] for i, key in enumerate(params['legend'])}
 
 
+# lateral neural network feedforward
+class FluxModel:
+  # dict used to rename activation functions whose names aren't valid python identifiers
+  activation_function_names = {'σ': 'sigmoid'}
+  def __init__(self, params_file, zero_bias=False):
+    with open(params_file, "r") as f:
+      params = load(f)
+
+    self.input_size = params["input_size"]
+    self.output_size = params["output_size"]
+    self.input_mean = np.array(params["input_mean"], dtype=np.float32).T
+    self.input_std = np.array(params["input_std"], dtype=np.float32).T
+    self.layers = []
+
+    for layer_params in params["layers"]:
+      W = np.array(layer_params[next(key for key in layer_params.keys() if key.endswith('_W'))], dtype=np.float32).T
+      b = np.array(layer_params[next(key for key in layer_params.keys() if key.endswith('_b'))], dtype=np.float32).T
+      if zero_bias:
+        b = np.zeros_like(b)
+      activation = layer_params["activation"]
+      for k, v in self.activation_function_names.items():
+        activation = activation.replace(k, v)
+      self.layers.append((W, b, activation))
+    
+    self.validate_layers()
+    
+  # Begin activation functions.
+  # These are called by name using the keys in the model json file
+  def sigmoid(self, x):
+    return 1 / (1 + np.exp(np.clip(-x, -np.inf, 709)))
+
+  def identity(self, x):
+    return x
+  # End activation functions
+
+  def forward(self, x):
+    for W, b, activation in self.layers:
+      x = getattr(self, activation)(x.dot(W) + b)
+    return x
+
+  def evaluate(self, input_array):
+    in_len = len(input_array)
+    if in_len != self.input_size:
+      # If the input is length 2-4, then it's a simplified evaluation.
+      # In that case, need to add on zeros to fill out the input array to match the correct length.
+      if 2 <= in_len:
+        input_array = input_array + [0] * (self.input_size - in_len)
+      else:
+        raise ValueError(f"Input array length {len(input_array)} must be length 2 or greater")
+        
+    input_array = np.array(input_array, dtype=np.float32)#.reshape(1, -1)
+
+    # Rescale the input array using the input_mean and input_std
+    input_array = (input_array - self.input_mean) / self.input_std
+
+    output_array = self.forward(input_array)
+
+    return float(output_array[0, 0])
+  
+  def validate_layers(self):
+    for W, b, activation in self.layers:
+      if not hasattr(self, activation):
+        raise ValueError(f"Unknown activation: {activation}")
+  
+def get_nn_ff_model_path(car):
+  return f"/data/openpilot/selfdrive/car/torque_data/lat_models/{car}.json"
+
+def has_nn_ff(car):
+  model_path = get_nn_ff_model_path(car)
+  if os.path.isfile(model_path):
+    return True
+  else:
+    return False
+  
+def initialize_nnff(car) -> Union[FluxModel, None]:
+  model = None
+  if has_nn_ff(car):
+    model = FluxModel(get_nn_ff_model_path(car))
+  return model
+
 # generic car and radar interfaces
 
 class CarInterfaceBase(ABC):
   def __init__(self, CP, CarController, CarState):
     self.CP = CP
     self.VM = VehicleModel(CP)
+    self.has_lateral_torque_nnff = self.initialize_lat_torque_nnff(CP.carFingerprint)
 
     self.frame = 0
     self.steering_unpressed = 0
@@ -83,6 +166,13 @@ class CarInterfaceBase(ABC):
     self.CC = None
     if CarController is not None:
       self.CC = CarController(self.cp.dbc_name, CP, self.VM)
+      
+  def get_ff_nn(self, x):
+    return self.lat_torque_nnff_model.evaluate(x)
+  
+  def initialize_lat_torque_nnff(self, car):
+    self.lat_torque_nnff_model = initialize_nnff(car)
+    return (self.lat_torque_nnff_model is not None)
 
   @staticmethod
   def get_pid_accel_limits(CP, current_speed, cruise_speed):
@@ -99,6 +189,9 @@ class CarInterfaceBase(ABC):
   def get_params(cls, candidate: str, fingerprint: Dict[int, Dict[int, int]], car_fw: List[car.CarParams.CarFw], experimental_long: bool, docs: bool):
     ret = CarInterfaceBase.get_std_params(candidate)
     ret = cls._get_params(ret, candidate, fingerprint, car_fw, experimental_long, docs)
+    
+    # Enable torque controller for all cars
+    CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     # Set common params using fields set by the car interface
     # TODO: get actual value, for now starting with reasonable value for