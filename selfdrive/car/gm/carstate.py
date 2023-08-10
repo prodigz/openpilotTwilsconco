e7ac51f2d26e75b288573951c809eff057f59875
prodigz <32366732+prodigz@users.noreply.github.com>
2023-08-09 18:52:16 -0600

    Update carstate.py

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -28,7 +28,7 @@ class CarState(CarStateBase):
     self.buttons_counter = 0
     
     self.regenPaddlePressed = False
-    self.autoHold = CP.carFingerprint == CAR.VOLT
+    self.autoHold = False
     self.autoHoldActive = False
     self.autoHoldActivated = False
     self.lastAutoHoldTime = 0.0
